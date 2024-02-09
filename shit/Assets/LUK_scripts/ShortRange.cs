a65732a824642683328812cc567b12f5f87ee76c
Hannah :3 <123725147+cheezbqlls@users.noreply.github.com>
2024-02-09 09:46:21 +0100

    ussy

---
+++ shit/Assets/LUK_scripts/ShortRange.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortRange : MonoBehaviour
+{
+    public Animator animator;
+
+    public float attackRange = 1f;
+
+    public string compareTag;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        // Play attack animation
+        animator.SetTrigger("ShortRange");
+
+
+        // Detect enemies in range
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, attackRange);
+
+        // Damage enemies
+        foreach (Collider c in hitColliders)
+        {
+            if (c.CompareTag(compareTag))
+            {
+                c.GetComponent<Health>()?.Damage(10);
+            }
+        }
+    }
+ }
